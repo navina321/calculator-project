e059e352ce66e8abb2944e62321c758c16118bfc
navina321 <81630548+navina321@users.noreply.github.com>
2023-01-24 11:53:10 +0000

    declared variables in javascript file

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// declare variables
+var displayOne = document.querySelector(".display-1");
+var displayTwo = document.querySelector(".display-2");
+var numbers = document.querySelectorAll(".number");
+var operation = document.querySelectorAll(".operator");
+var equals = document.querySelector(".equal");
+var clearAll = document.querySelector(".all-clear");
+var deleteLast = document.querySelector(".delete");
+var display1 = "";
+var display2 = "";
+var result = null;
+var lastOperation = "";
+var hasDecimal = false;
