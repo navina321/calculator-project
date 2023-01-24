a7040bb95a2df9b3ea4491d3728fa6d8043a2db2
navina321 <81630548+navina321@users.noreply.github.com>
2023-01-24 11:55:52 +0000

    added function for clearing display2 and mathOperation

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -12,4 +12,24 @@ var display1 = "";
 var display2 = "";
 var result = null;
 var lastOperation = "";
-var hasDecimal = false;
+var hasDecimal = false; //function to clear variable from display2 (move from display2 to 1)
+
+var clearVariable = function clearVariable(nameOfOp) {
+  display1 += display2 + " " + nameOfOp + " ";
+  displayOne.innerText = display1;
+  displayTwo.innerText = "";
+  display2 = "";
+}; //function for math operations
+
+
+var mathOperation = function mathOperation() {
+  if (lastOperation === "X") {
+    result = parseFloat(result) * parseFloat(display2);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(display2);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(display2);
+  } else if (lastOperation === "÷") {
+    result = parseFloat(result) / parseFloat(display2);
+  }
+};
--- main.js
+++ main.js
@@ -12,3 +12,24 @@ let display2 = "";
 let result = null;
 let lastOperation = "";
 let hasDecimal = false;
+
+//function to clear variable from display2 (move from display2 to 1)
+const clearVariable = (nameOfOp) => {
+    display1 += display2 + " " + nameOfOp + " ";
+    displayOne.innerText = display1
+    displayTwo.innerText = "";
+    display2 = "";
+}
+
+//function for math operations
+const mathOperation = () => {
+    if (lastOperation === "X") {
+        result = parseFloat(result) * parseFloat(display2);
+    } else if (lastOperation === "+") {
+        result = parseFloat(result) + parseFloat(display2);
+    } else if (lastOperation === "-") {
+        result = parseFloat(result) - parseFloat(display2);
+    } else if (lastOperation === "÷") {
+        result = parseFloat(result) / parseFloat(display2);
+    } 
+};
