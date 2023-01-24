1cf7d68622163fee2ef527a1cf8cef2a46fe48af
navina321 <81630548+navina321@users.noreply.github.com>
2023-01-24 00:33:15 +0000

    changed class names in html added css

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,9 +1,3 @@
 "use strict";
 
-var numberButtons = document.querySelectorAll("[data-number]");
-var operatorButtons = document.querySelectorAll("[data-operation]");
-var equalsButton = document.querySelector("data-equals");
-var deleteButton = document.querySelector("data-delete");
-var allClearButton = document.querySelector("data-all-clear");
-var previousTextElement = document.querySelector("data-previous");
-var currentTextElement = document.querySelector("data-current");
+console.log("Hello");
--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const numberButtons = document.querySelectorAll("[data-number]")
-const operatorButtons = document.querySelectorAll("[data-operation]")
-const equalsButton = document.querySelector("data-equals")
-const deleteButton = document.querySelector("data-delete")
-const allClearButton = document.querySelector("data-all-clear")
-const previousTextElement = document.querySelector("data-previous")
-const currentTextElement = document.querySelector("data-current")
+console.log("Hello");
+
 
